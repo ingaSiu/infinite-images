e9b21392313b6ae196052ca146422e898ea81348
ingaSiu <ingasiud@gmail.com>
2023-08-28 20:14:22 +0300

    add basic functionality to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import { getImagesPaginated } from './api/images';
 
 const App = () => {
-  
+  const [images, setImages] = useState([]);
+
+  const getImages = () => {
+    getImagesPaginated().then((photos) => {
+      console.log(photos);
+      setImages(photos);
+    });
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
 
   return (
- 
-     <div>
-      <h1>Hello</h1>
-     </div>
-    
-  )
-}
+    <div>
+      {images.length > 0 && (
+        <ul>
+          {images.map((item) => (
+            <li key={item.id}>{item.photographer}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
+
