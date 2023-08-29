7c302bf983e2931c89d3a4418edec76616e6c3cf
ingaSiu <ingasiud@gmail.com>
2023-08-29 12:04:14 +0300

    add basic card structure

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -8,10 +8,15 @@ type CardProps = {
   src: string;
 };
 
-const Card = ({ alt, src }: CardProps) => {
+const Card = ({ alt, src, photographer }: CardProps) => {
   return (
     <div className={styles.container}>
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} loading="lazy" className={styles.image} />
+      <div className={styles.overlay}>
+        <div className={styles.title}>{alt}</div>
+        <div className={styles.author}>{photographer}</div>
+        <button>Favorite</button>
+      </div>
     </div>
   );
 };
