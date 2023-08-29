08b7dc28a56386159013e899e5f163c425a99f63
ingaSiu <ingasiud@gmail.com>
2023-08-29 10:52:35 +0300

    move functionality to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-import { useEffect, useState } from 'react';
-
-import { PexelsImage } from './types/images';
-import { getImagesPaginated } from './api/images';
+import Home from './Pages/Home';
 
 const App = () => {
-  const [images, setImages] = useState<PexelsImage[]>([]);
-
-  const getImages = () => {
-    getImagesPaginated().then((photos) => {
-      console.log(photos);
-      setImages(photos);
-    });
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
-
-  return (
-    <div>
-      {images.length > 0 && (
-        <ul>
-          {images.map((item) => (
-            <li key={item.id}>{item.photographer}</li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
