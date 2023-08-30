1992128cf0f8ea4d27909334860e52706f990b29
ingaSiu <ingasiud@gmail.com>
2023-08-30 09:57:39 +0300

    add intersection observer for infinite-scrolling functionality

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Card from '../components/card/Card';
 import { PexelsImage } from '../types/images';
@@ -8,34 +8,49 @@ import styles from './Home.module.scss';
 const Home = () => {
   const [images, setImages] = useState<PexelsImage[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+
   const [page, setPage] = useState(1);
+
+  const bottom = useRef<HTMLDivElement | null>(null);
+
+  //workaround for strict mode for useffect firing twice
+  const initialized = useRef(false);
 
   const getImages = () => {
     setIsLoading(true);
-
-    getImagesPaginated(page).then((photos) => {
+    return getImagesPaginated(page).then((photos) => {
       console.log(photos);
 
+      //setImages((prevItems) => [...new Set([...prevItems, ...photos])]);
       setImages((prevItems) => [...prevItems, ...photos]);
-
       setIsLoading(false);
     });
   };
 
-  const handleScroll = () => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight ||
-      isLoading
-    ) {
-      return;
+  useEffect(() => {
+    //will trigger only on 1st useeffect - page load
+    if (!initialized.current) {
+      console.log('initial load effect logic');
+      initialized.current = true;
+      getImages().then(() => {
+        const observer = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting) {
+            console.log('is intersecting. increasing page');
+            setPage((prevPage) => prevPage + 1);
+          }
+        });
+        if (bottom.current) {
+          observer.observe(bottom.current);
+        }
+      });
     }
-    setPage((prevPage) => prevPage + 1);
-  };
+  }, []);
 
   useEffect(() => {
-    getImages();
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    if (page > 1) {
+      console.log('page change effect logic');
+      getImages();
+    }
   }, [page]);
 
   return (
@@ -48,6 +63,7 @@ const Home = () => {
         </div>
       )}
       {isLoading && <p>Loading...</p>}
+      <div ref={bottom}></div>
     </div>
   );
 };
