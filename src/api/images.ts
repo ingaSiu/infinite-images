3e919227f60b66bf73a9bb8e73a5053d12a9b076
ingaSiu <ingasiud@gmail.com>
2023-09-01 08:26:08 +0300

    rewrite to async/await

--- src/api/images.ts
+++ src/api/images.ts
@@ -4,16 +4,12 @@ const BASE_URL = 'https://api.pexels.com/v1/';
 
 const KEY = 'rXHEVgX4HhHjBUUyjdjobJlrsW0MX3OZDWMCCHWM8nQRdDQE5Bk72Hfx';
 
-export const getImagesPaginated = (page: number = 1, perPage: number = 40): Promise<PexelsImage[]> => {
-  return fetch(`${BASE_URL}curated?page=${page}&per_page=${perPage}`, {
+export const getImagesPaginated = async (page: number = 1, perPage: number = 40): Promise<PexelsImage[]> => {
+  const response = await fetch(`${BASE_URL}curated?page=${page}&per_page=${perPage}`, {
     headers: {
       Authorization: KEY,
     },
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      return data.photos;
-    });
+  });
+  const data = await response.json();
+  return data.photos;
 };
