664ef16d28ae4ff92c8bfde52153f078c22dffa5
ingaSiu <ingasiud@gmail.com>
2023-09-03 12:37:34 +0300

    add err msg, some small fixes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -12,8 +12,11 @@ const Home = () => {
   const [images, setImages] = useState<PexelsImage[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
+  const prevPage = useRef(0);
 
   const [likedPhotos, setLikedPhotos] = useState<number[]>([]);
+
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   const bottom = useRef<HTMLDivElement | null>(null);
 
@@ -38,15 +41,19 @@ const Home = () => {
       console.log(photos);
       //check if api returned duplicates and remove if any
       const filteredPhotos = photos.filter((photo) => !images.some((image) => image.id === photo.id));
-      setImages((prevItems) => [...prevItems, ...filteredPhotos]);
-      setIsLoading(false);
+      setImages((currentItems) => [...currentItems, ...filteredPhotos]);
     } catch (err) {
       if (page > 1) {
-        setPage((prevItem) => prevItem - 1);
+        setPage((currentPage) => currentPage - 1);
       }
       //TODO add some UI error
       console.error('An error occurred while fetching images:', err);
-      // setErrorState(err.message); can make err msg
+      setErrorMsg('Could not load images');
+      setTimeout(() => {
+        setErrorMsg(null);
+      }, 3000);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -59,7 +66,7 @@ const Home = () => {
         const observer = new IntersectionObserver((entries) => {
           if (entries[0].isIntersecting) {
             console.log('is intersecting. increasing page');
-            setPage((prevPage) => prevPage + 1);
+            setPage((currentPage) => currentPage + 1);
           }
         });
         if (bottom.current) {
@@ -75,10 +82,11 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    if (page > 1) {
+    if (page > 1 && prevPage.current < page) {
       console.log('page change effect logic');
       getImages();
     }
+    prevPage.current = page;
   }, [page]);
 
   useEffect(() => {
@@ -90,7 +98,7 @@ const Home = () => {
       <div className={styles.pageWrapper}>
         {images.length > 0 && (
           <div className={styles.imagesContainer}>
-            {images.map((item) => (
+            {images.map((item, index) => (
               <Card
                 key={item.id}
                 src={item.src.large}
@@ -98,16 +106,15 @@ const Home = () => {
                 photographer={item.photographer}
                 onClick={() => handleFavourites(item.id)}
                 isClicked={likedPhotos.includes(item.id)}
+                tabIndex={index}
               />
             ))}
           </div>
         )}
-
         {isLoading && <Loader />}
-
-        {/*TODO add Load more button as a backup*/}
+        {errorMsg && <div className={styles.toast}>{errorMsg}</div>}
       </div>
-      <div ref={bottom}></div>
+      <div className={styles.intersectBox} ref={bottom}></div>
     </>
   );
 };
