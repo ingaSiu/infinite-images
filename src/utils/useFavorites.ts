7c871356aab250b29f322fa572488e84dfd314c5
ingaSiu <ingasiud@gmail.com>
2024-01-09 19:59:52 +0200

    fix the bug

--- src/utils/useFavorites.ts
+++ src/utils/useFavorites.ts
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const useFavorites = (storageKey: string) => {
   const [likedPhotos, setLikedPhotos] = useState<number[]>([]);
+  const initialized = useRef(false);
 
   const handleFavorites = (itemId: number) => {
+    console.log('handle favourites');
     if (likedPhotos.includes(itemId)) {
       setLikedPhotos(likedPhotos.filter((id) => id !== itemId));
       return;
@@ -12,9 +14,23 @@ const useFavorites = (storageKey: string) => {
   };
 
   useEffect(() => {
+    if (!initialized.current) {
+      const storedData = window.localStorage.getItem(storageKey);
+      if (storedData !== null) {
+        console.log('get');
+        console.log(JSON.parse(storedData));
+        setLikedPhotos(JSON.parse(storedData));
+      }
+    }
+    initialized.current = true;
+  }, [storageKey]);
+
+  useEffect(() => {
+    console.log('set');
+    console.log(likedPhotos);
     window.localStorage.setItem(storageKey, JSON.stringify(likedPhotos));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [likedPhotos]);
+  }, [likedPhotos, storageKey]);
 
   return { handleFavorites, likedPhotos, setLikedPhotos };
 };
