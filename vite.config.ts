6f21ace63cf8fb64dd476b7753a0bd5b14a2f9bc
ingaSiu <ingasiud@gmail.com>
2024-01-15 09:19:53 +0200

    change base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fe-vinted',
+  base: '/infinite-images',
   plugins: [react()],
 });
-
