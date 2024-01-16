a591e4e756023f2e30dc496f39fd9af09d8515e2
ingaSiu <ingasiud@gmail.com>
2024-01-16 20:05:15 +0200

    add base fetch method, create search img fetch

--- src/api/images.ts
+++ src/api/images.ts
@@ -5,7 +5,19 @@ const BASE_URL = 'https://api.pexels.com/v1/';
 const KEY = import.meta.env.VITE_ACCESS_KEY;
 
 export const getImagesPaginated = async (page: number = 1, perPage: number = 40): Promise<PexelsImage[]> => {
-  const response = await fetch(`${BASE_URL}curated?page=${page}&per_page=${perPage}`, {
+  return getPhotos(`${BASE_URL}curated?page=${page}&per_page=${perPage}`);
+};
+
+export const searchImagesPaginated = async (
+  query: string,
+  page: number = 1,
+  perPage: number = 40,
+): Promise<PexelsImage[]> => {
+  return getPhotos(`${BASE_URL}search?query=${query}&page=${page}&per_page=${perPage}`);
+};
+
+const getPhotos = async (fetchUrl: string): Promise<PexelsImage[]> => {
+  const response = await fetch(fetchUrl, {
     headers: {
       Authorization: KEY,
     },
