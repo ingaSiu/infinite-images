64bc2cbe980e063652cec35ad0c1fe4b489c0596
ingaSiu <ingasiud@gmail.com>
2024-09-03 11:26:27 +0300

    add some basic styling

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,11 +1,12 @@
 import RegisterForm from './RegisterForm';
+import styles from './Register.module.scss';
 
 const Register = () => {
   return (
-    <>
+    <div className={styles.container}>
       <div>Register</div>
       <RegisterForm />
-    </>
+    </div>
   );
 };
 
