85fbc7bca75ac57dd9c2ba1aef9aa6bbd5a6e332
ingaSiu <ingasiud@gmail.com>
2024-09-03 11:26:13 +0300

    add register user to db functionality

--- src/Pages/Register/RegisterForm.tsx
+++ src/Pages/Register/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
 
+import { BASE_URL } from '../../api/baseApi';
 import { EMAIL_REGEX } from '../../utils/regex';
 import axios from 'axios';
 import styles from './Register.module.scss';
@@ -25,34 +26,55 @@ const RegisterForm = () => {
     formState: { errors },
   } = useForm<RegisterFormData>({ resolver: yupResolver(schema) });
 
-  const onSubmit = () => {};
+  const onSubmit = async (data: RegisterFormData) => {
+    const { email, username, password } = data;
+
+    try {
+      await axios.post(
+        `${BASE_URL}users/`,
+        { email, username, password },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      alert('Registration succesful');
+    } catch (error) {
+      console.error(error);
+      alert('Registration failed. Please try again');
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
+      <div className={styles.inputContainer}>
         <label htmlFor="username">Username</label>
         <input type="text" {...register('username')} />
         {errors.username && <p className={styles.errorMsg}>{errors.username.message}</p>}
       </div>
 
-      <div>
+      <div className={styles.inputContainer}>
         <label htmlFor="email">Email</label>
         <input type="email" {...register('email')} />
         {errors.email && <p className={styles.errorMsg}>{errors.email.message}</p>}
       </div>
 
-      <div>
+      <div className={styles.inputContainer}>
         <label htmlFor="password">Password</label>
         <input type="password" {...register('password')} />
         {errors.password && <p className={styles.errorMsg}>{errors.password.message}</p>}
       </div>
 
-      <div>
+      <div className={styles.inputContainer}>
         <label>Confirm Password</label>
         <input type="password" {...register('confirmPassword')} />
         {errors.confirmPassword && <p className={styles.errorMsg}>{errors.confirmPassword.message}</p>}
       </div>
 
-      <button type="submit">Register</button>
+      <button className={styles.registerBtn} type="submit">
+        Register
+      </button>
     </form>
   );
 };
