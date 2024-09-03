037d82eff11e093ac8933d94fe419e6cbd18517c
ingaSiu <ingasiud@gmail.com>
2024-09-03 11:10:39 +0300

    add form validation and useForm hook

--- src/Pages/Register/RegisterForm.tsx
+++ src/Pages/Register/RegisterForm.tsx
@@ -1,31 +1,58 @@
-// import * as yup from 'yup';
+import * as yup from 'yup';
 
-// import { useForm } from 'react-hook-form';
+import { EMAIL_REGEX } from '../../utils/regex';
+import axios from 'axios';
+import styles from './Register.module.scss';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup.object().shape({
+  username: yup.string().required('Username field is required.'),
+  email: yup.string().required('Email is required.').matches(EMAIL_REGEX, 'Invalid email address.'),
+  password: yup.string().required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .required('Please confirm your password')
+    .oneOf([yup.ref('password')], 'Your passwords do not match.'),
+});
+
+type RegisterFormData = yup.InferType<typeof schema>;
 
 const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({ resolver: yupResolver(schema) });
+
+  const onSubmit = () => {};
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="username">Username</label>
-        <input type="text" />
+        <input type="text" {...register('username')} />
+        {errors.username && <p className={styles.errorMsg}>{errors.username.message}</p>}
       </div>
 
       <div>
         <label htmlFor="email">Email</label>
-        <input type="email" />
+        <input type="email" {...register('email')} />
+        {errors.email && <p className={styles.errorMsg}>{errors.email.message}</p>}
       </div>
 
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" />
+        <input type="password" {...register('password')} />
+        {errors.password && <p className={styles.errorMsg}>{errors.password.message}</p>}
       </div>
 
       <div>
         <label>Confirm Password</label>
-        <input type="password" />
+        <input type="password" {...register('confirmPassword')} />
+        {errors.confirmPassword && <p className={styles.errorMsg}>{errors.confirmPassword.message}</p>}
       </div>
 
-      <button>Register</button>
+      <button type="submit">Register</button>
     </form>
   );
 };
