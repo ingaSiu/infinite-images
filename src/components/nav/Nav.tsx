3f1c37a2fbfd36d75dab0dd78ad9b97d0dee5be5
ingaSiu <ingasiud@gmail.com>
2024-09-04 09:05:22 +0300

    remove search bar

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,12 +1,9 @@
-import SearchBar, { Props } from '../search/SearchBar';
-
 import styles from './Nav.module.scss';
 
-const Nav = ({ onSearch }: Props) => {
+const Nav = () => {
   return (
     <div className={styles.navContainer}>
       <h2>Images viewing website</h2>
-      <SearchBar onSearch={onSearch} />
     </div>
   );
 };
