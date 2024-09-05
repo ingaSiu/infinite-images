dd0640434882143031165c79b4b40cc55695ff64
ingaSiu <ingasiud@gmail.com>
2024-09-05 10:41:59 +0300

    update login page with img

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,10 +1,14 @@
 import LoginForm from './LoginForm';
+import styles from './Login.module.scss';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <LoginForm />
+    <div className={styles.wrapper}>
+      <h1>Login Here!</h1>
+      <div className={styles.container}>
+        <img src="./cat_login.png" alt="cat with red baloon" />
+        <LoginForm />
+      </div>
     </div>
   );
 };
