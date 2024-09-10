d53d4316dda9cde84de895cc8cd2bf3bf3ed8ff4
ingaSiu <ingasiud@gmail.com>
2024-09-10 12:58:16 +0300

    add user page elements

--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -1,5 +1,16 @@
+import styles from './UserPage.module.scss';
+
 const UserPage = () => {
-  return <div>UserPage</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Favorite images</h1>
+      <div className={styles.cardsWrapper}>
+        <div className={styles.card}>card</div>
+        <div className={styles.card}>card</div>
+        <div className={styles.card}>card</div>
+      </div>
+    </div>
+  );
 };
 
 export default UserPage;
