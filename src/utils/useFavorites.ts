77e9a1596a992251233a0c76ce24d5d3be136533
ingaSiu <ingasiud@gmail.com>
2024-09-18 11:03:42 +0300

    rewrite hook for authenticated users

--- src/utils/useFavorites.ts
+++ src/utils/useFavorites.ts
@@ -1,15 +1,35 @@
 import { useEffect, useRef, useState } from 'react';
 
+import { BASE_URL } from '../api/baseApi';
+import httpClient from '../api/httpClient';
+import { useAuthContext } from '../hooks/useAuthContext';
+
 const useFavorites = (storageKey: string) => {
   const [likedPhotos, setLikedPhotos] = useState<number[]>([]);
   const initialized = useRef(false);
+  const { user, isAuthenticated } = useAuthContext();
 
-  const handleFavorites = (itemId: number) => {
-    if (likedPhotos.includes(itemId)) {
-      setLikedPhotos(likedPhotos.filter((id) => id !== itemId));
-      return;
+  const handleFavorites = async (itemId: number, imageUrl: string) => {
+    if (isAuthenticated && user) {
+      try {
+        if (likedPhotos.includes(itemId)) {
+          setLikedPhotos(likedPhotos.filter((id) => id !== itemId));
+          return;
+        } else {
+          setLikedPhotos((prevLikedPhotos) => [...prevLikedPhotos, itemId]);
+
+          await httpClient.post(`${BASE_URL}users/favorites`, { id: itemId, url: imageUrl }, { withCredentials: true });
+        }
+      } catch (error) {
+        console.error('Error adding favorite', error);
+      }
+    } else {
+      if (likedPhotos.includes(itemId)) {
+        setLikedPhotos(likedPhotos.filter((id) => id !== itemId));
+      } else {
+        setLikedPhotos((prevLikedPhotos) => [...prevLikedPhotos, itemId]);
+      }
     }
-    setLikedPhotos((prevLikedPhotos) => [...prevLikedPhotos, itemId]);
   };
 
   useEffect(() => {
