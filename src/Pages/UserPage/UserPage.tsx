a1c440bf837e9963671904bc4df8d97a1b36f0dd
ingaSiu <ingasiud@gmail.com>
2024-09-20 11:46:52 +0300

    map favorites in users page

--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -1,13 +1,24 @@
+import Card from '../../components/card/Card';
 import styles from './UserPage.module.scss';
+import { useUserFavorites } from '../../hooks/useUserFavorites';
 
 const UserPage = () => {
+  const { favorites } = useUserFavorites();
   return (
     <div className={styles.container}>
+      <section>
+        <span>My favorites</span>
+        <span>My profile</span>
+      </section>
       <h1>Favorite images</h1>
       <div className={styles.cardsWrapper}>
-        <div className={styles.card}>card</div>
-        <div className={styles.card}>card</div>
-        <div className={styles.card}>card</div>
+        {favorites.length > 0 ? (
+          favorites.map((fav, index) => (
+            <Card key={fav.id} src={fav.src.large} alt={fav.alt} photographer={fav.photographer} tabIndex={index} />
+          ))
+        ) : (
+          <p>No favorites yet!</p>
+        )}
       </div>
     </div>
   );
