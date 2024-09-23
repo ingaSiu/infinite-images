02482d3ab1cc756c713f6203c577c205c20f28aa
ingaSiu <ingasiud@gmail.com>
2024-09-23 09:11:12 +0300

    add another route

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -1,4 +1,4 @@
 export type Route = {
-  path: '/' | '/login' | '/register' | '/users/profile/:id';
+  path: '/' | '/login' | '/register' | '/profile/:id' | '/profile/:id/favorites';
   Component: () => JSX.Element;
 };
