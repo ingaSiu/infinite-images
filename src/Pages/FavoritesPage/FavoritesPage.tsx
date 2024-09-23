306b360c969d9e7a40e09b2f15b9cf96fcd12947
ingaSiu <ingasiud@gmail.com>
2024-09-23 09:08:26 +0300

    move code to favorites page

---
+++ src/Pages/FavoritesPage/FavoritesPage.tsx
@@ -0,0 +1,27 @@
+import Card from '../../components/card/Card';
+import styles from './FavoritesPage.module.scss';
+import { useUserFavorites } from '../../hooks/useUserFavorites';
+
+const FavoritesPage = () => {
+  const { favorites } = useUserFavorites();
+  return (
+    <div className={styles.container}>
+      <section>
+        <span>My favorites</span>
+        <span>My profile</span>
+      </section>
+      <h1>Favorite images</h1>
+      <div className={styles.cardsWrapper}>
+        {favorites.length > 0 ? (
+          favorites.map((fav, index) => (
+            <Card key={fav.id} src={fav.src.large} alt={fav.alt} photographer={fav.photographer} tabIndex={index} />
+          ))
+        ) : (
+          <p>No favorites yet!</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FavoritesPage;
