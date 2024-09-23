43465dfaebb11d0be8e83c7f099989ebe26b9b3c
ingaSiu <ingasiud@gmail.com>
2024-09-23 09:08:39 +0300

    create another users page

--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -1,25 +1,13 @@
-import Card from '../../components/card/Card';
 import styles from './UserPage.module.scss';
-import { useUserFavorites } from '../../hooks/useUserFavorites';
 
 const UserPage = () => {
-  const { favorites } = useUserFavorites();
   return (
     <div className={styles.container}>
       <section>
         <span>My favorites</span>
         <span>My profile</span>
       </section>
-      <h1>Favorite images</h1>
-      <div className={styles.cardsWrapper}>
-        {favorites.length > 0 ? (
-          favorites.map((fav, index) => (
-            <Card key={fav.id} src={fav.src.large} alt={fav.alt} photographer={fav.photographer} tabIndex={index} />
-          ))
-        ) : (
-          <p>No favorites yet!</p>
-        )}
-      </div>
+      <h1>My profile details</h1>
     </div>
   );
 };
