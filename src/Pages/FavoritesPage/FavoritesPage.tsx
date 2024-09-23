44b22474a6d4045db2cd5bec7a6a155058c62c44
ingaSiu <ingasiud@gmail.com>
2024-09-23 09:25:28 +0300

    add simple navigation in users pages

--- src/Pages/FavoritesPage/FavoritesPage.tsx
+++ src/Pages/FavoritesPage/FavoritesPage.tsx
@@ -1,14 +1,18 @@
+import { FAVORITES_PATH, USER_PATH } from '../../routes/consts';
+
 import Card from '../../components/card/Card';
 import styles from './FavoritesPage.module.scss';
+import { useNavigate } from 'react-router-dom';
 import { useUserFavorites } from '../../hooks/useUserFavorites';
 
 const FavoritesPage = () => {
   const { favorites } = useUserFavorites();
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
-      <section>
-        <span>My favorites</span>
-        <span>My profile</span>
+      <section className={styles.userPages}>
+        <p onClick={() => navigate(FAVORITES_PATH)}>My favorites</p>
+        <p onClick={() => navigate(USER_PATH)}>My profile</p>
       </section>
       <h1>Favorite images</h1>
       <div className={styles.cardsWrapper}>
--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -1,11 +1,15 @@
+import { FAVORITES_PATH, USER_PATH } from '../../routes/consts';
+
 import styles from './UserPage.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const UserPage = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
-      <section>
-        <span>My favorites</span>
-        <span>My profile</span>
+      <section className={styles.userPages}>
+        <p onClick={() => navigate(FAVORITES_PATH)}>My favorites</p>
+        <p onClick={() => navigate(USER_PATH)}>My profile</p>
       </section>
       <h1>My profile details</h1>
     </div>
