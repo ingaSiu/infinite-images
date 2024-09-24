f8802c256bc975d94c5cb41363c4ab339d046337
ingaSiu <ingasiud@gmail.com>
2024-09-24 11:36:45 +0300

    add card component to user fav page

--- src/Pages/FavoritesPage/FavoritesPage.tsx
+++ src/Pages/FavoritesPage/FavoritesPage.tsx
@@ -2,27 +2,40 @@ import { FAVORITES_PATH, USER_PATH } from '../../routes/consts';
 
 import Card from '../../components/card/Card';
 import styles from './FavoritesPage.module.scss';
+import useFavorites from '../../utils/useFavorites';
 import { useNavigate } from 'react-router-dom';
 import { useUserFavorites } from '../../hooks/useUserFavorites';
 
 const FavoritesPage = () => {
   const { favorites } = useUserFavorites();
+  const { deleteFavorite } = useFavorites('likedPhotos');
   const navigate = useNavigate();
   return (
-    <div className={styles.container}>
+    <div>
       <section className={styles.userPages}>
         <p onClick={() => navigate(FAVORITES_PATH)}>My favorites</p>
         <p onClick={() => navigate(USER_PATH)}>My profile</p>
       </section>
-      <h1>Favorite images</h1>
-      <div className={styles.cardsWrapper}>
-        {favorites.length > 0 ? (
-          favorites.map((fav, index) => (
-            <Card key={fav.id} src={fav.src.large} alt={fav.alt} photographer={fav.photographer} tabIndex={index} />
-          ))
-        ) : (
-          <p>No favorites yet!</p>
-        )}
+
+      <div className={styles.container}>
+        <h1>Favorite images</h1>
+        <div className={styles.cardsWrapper}>
+          {favorites.length > 0 ? (
+            favorites.map((fav, index) => (
+              <Card
+                key={fav.id}
+                src={fav.src.large}
+                alt={fav.alt}
+                photographer={fav.photographer}
+                tabIndex={index}
+                isClicked={true}
+                onClick={() => deleteFavorite(fav.id)}
+              />
+            ))
+          ) : (
+            <p>No favorites yet!</p>
+          )}
+        </div>
       </div>
     </div>
   );
