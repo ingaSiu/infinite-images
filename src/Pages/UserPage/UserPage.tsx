307e4625575a5d66a038cc06062e6cd282b3bce4
ingaSiu <ingasiud@gmail.com>
2024-10-14 11:08:29 +0300

    import edit form to user page

--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -1,42 +1,19 @@
 import { FAVORITES_PATH, USER_PATH } from '../../routes/consts';
 
-import Button from '../../components/button/Button';
+import UpdateUserForm from './UpdateUserForm';
 import styles from './UserPage.module.scss';
-import { useAuthContext } from '../../hooks/useAuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const UserPage = () => {
-  const { user } = useAuthContext();
   const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
       <section className={styles.userPages}>
         <p onClick={() => navigate(FAVORITES_PATH)}>My favorites</p>
         <p onClick={() => navigate(USER_PATH)}>My profile</p>
       </section>
-
-      <div>
-        <h1>My profile details</h1>
-        <form>
-          <div className={styles.inputContainer}>
-            <input type="text" placeholder="Username" />
-          </div>
-
-          <div className={styles.inputContainer}>
-            <input type="email" placeholder="Email" />
-          </div>
-
-          <div className={styles.inputContainer}>
-            <input type="password" placeholder="Password" />
-          </div>
-
-          <div className={styles.inputContainer}>
-            <input type="password" placeholder="Confirm password" />
-          </div>
-
-          <Button variant={true}>Update</Button>
-        </form>
-      </div>
+      <UpdateUserForm />
     </div>
   );
 };
