d30e6cd402fab76c24e94265bf76f81f8f63c0d3
ingaSiu <ingasiud@gmail.com>
2024-10-14 09:30:14 +0300

    add navigation to register in login form

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,13 +1,21 @@
 import LoginForm from './LoginForm';
+import { REGISTER_PATH } from '../../routes/consts';
 import styles from './Login.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.wrapper}>
       <h1>Login Here!</h1>
       <div className={styles.container}>
         <img src="./cat_login.png" alt="sitting cat with heart above its head" />
-        <LoginForm />
+
+        <div className={styles.formContainer}>
+          <LoginForm />
+          <span>Don't have an account?</span>
+          <p onClick={() => navigate(REGISTER_PATH)}>Sign up</p>
+        </div>
       </div>
     </div>
   );
