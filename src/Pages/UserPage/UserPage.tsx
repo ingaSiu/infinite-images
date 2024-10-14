84a47c54b5a51c121b87ca04ef6d33e88b38fac8
ingaSiu <ingasiud@gmail.com>
2024-10-14 09:45:00 +0300

    add simple form to user page

--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -1,9 +1,12 @@
 import { FAVORITES_PATH, USER_PATH } from '../../routes/consts';
 
+import Button from '../../components/button/Button';
 import styles from './UserPage.module.scss';
+import { useAuthContext } from '../../hooks/useAuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const UserPage = () => {
+  const { user } = useAuthContext();
   const navigate = useNavigate();
   return (
     <div className={styles.container}>
@@ -11,7 +14,29 @@ const UserPage = () => {
         <p onClick={() => navigate(FAVORITES_PATH)}>My favorites</p>
         <p onClick={() => navigate(USER_PATH)}>My profile</p>
       </section>
-      <h1>My profile details</h1>
+
+      <div>
+        <h1>My profile details</h1>
+        <form>
+          <div className={styles.inputContainer}>
+            <input type="text" placeholder="Username" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <input type="email" placeholder="Email" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <input type="password" placeholder="Password" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <input type="password" placeholder="Confirm password" />
+          </div>
+
+          <Button variant={true}>Update</Button>
+        </form>
+      </div>
     </div>
   );
 };
