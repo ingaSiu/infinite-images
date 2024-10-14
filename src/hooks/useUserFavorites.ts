b9fa9534d56c1235fd211c66d5ba52c331d34575
ingaSiu <ingasiud@gmail.com>
2024-10-14 12:26:05 +0300

    set favorite ids to local storage

--- src/hooks/useUserFavorites.ts
+++ src/hooks/useUserFavorites.ts
@@ -1,23 +1,8 @@
 import { useEffect, useState } from 'react';
 
 import { BASE_URL } from '../api/baseApi';
+import { FavoritesProp } from '../types/favorites';
 import httpClient from '../api/httpClient';
-
-type FavoritesProp = {
-  alt: string;
-  id: number;
-  photographer: string;
-  src: {
-    original: string;
-    large2x: string;
-    large: string;
-    medium: string;
-    small: string;
-    portrait: string;
-    landscape: string;
-    tiny: string;
-  };
-};
 
 export const useUserFavorites = () => {
   const [favorites, setFavorites] = useState<FavoritesProp[]>([]);
@@ -30,7 +15,14 @@ export const useUserFavorites = () => {
       const { data } = await httpClient.get(`${BASE_URL}users/favorites`, {
         withCredentials: true,
       });
+
       setFavorites(data);
+
+      const favoriteIds = data.map((favorite: FavoritesProp) => favorite.id);
+      window.localStorage.setItem('favorites', JSON.stringify(favoriteIds));
+
+      console.log('Favorites updated:', data);
+
       setIsLoading(false);
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
