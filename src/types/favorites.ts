279bf1e14bf21f146f27995ed017f0b9329bbc59
ingaSiu <ingasiud@gmail.com>
2024-10-14 12:25:32 +0300

    create favorites type

---
+++ src/types/favorites.ts
@@ -0,0 +1,15 @@
+export type FavoritesProp = {
+  alt: string;
+  id: number;
+  photographer: string;
+  src: {
+    original: string;
+    large2x: string;
+    large: string;
+    medium: string;
+    small: string;
+    portrait: string;
+    landscape: string;
+    tiny: string;
+  };
+};
