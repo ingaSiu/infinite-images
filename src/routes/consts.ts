5999b36d867bc909db9f2afcfd09bf514fd31940
ingaSiu <ingasiud@gmail.com>
2024-10-16 10:16:18 +0300

    add user routes and user layout

--- src/routes/consts.ts
+++ src/routes/consts.ts
@@ -3,6 +3,7 @@ import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import MainLayout from '../layout/MainLayout';
 import Register from '../Pages/Register/Register';
+import UserLayout from '../layout/UserLayout';
 import UserPage from '../Pages/UserPage/UserPage';
 
 export const HOME_PATH = '/';
@@ -17,6 +18,12 @@ export const mainLayoutRoutes = {
     { path: HOME_PATH, Component: Home },
     { path: REGISTER_PATH, Component: Register },
     { path: LOGIN_PATH, Component: Login },
+  ],
+};
+
+export const userLayoutRoutes = {
+  Layout: UserLayout,
+  userRoutes: [
     { path: USER_PATH, Component: UserPage },
     { path: FAVORITES_PATH, Component: FavoritesPage },
   ],
