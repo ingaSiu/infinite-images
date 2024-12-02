e701e62b296db35c1dafeffba66f20d4874f2d7f
ingaSiu <ingasiud@gmail.com>
2024-12-02 12:50:23 +0200

    made reusable element for styling links

--- src/components/userNav/UserNav.tsx
+++ src/components/userNav/UserNav.tsx
@@ -4,12 +4,13 @@ import { NavLink } from 'react-router-dom';
 import styles from './UserNav.module.scss';
 
 const UserNav = () => {
+  const isElementActive = ({ isActive }: { isActive: boolean }) => ({ color: isActive ? '#ebac96' : '' });
   return (
     <section className={styles.userPages}>
-      <NavLink to={FAVORITES_PATH} end style={({ isActive }) => ({ color: isActive ? '#ebac96' : '' })}>
+      <NavLink to={FAVORITES_PATH} end style={isElementActive}>
         My Favorites
       </NavLink>
-      <NavLink to={USER_PATH} end style={({ isActive }) => ({ color: isActive ? '#ebac96' : '' })}>
+      <NavLink to={USER_PATH} end style={isElementActive}>
         My profile
       </NavLink>
     </section>
